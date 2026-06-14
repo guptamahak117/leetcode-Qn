acfe4171cdf9099e4ce1f21265960cc22e278adc
guptamahak117 <guptamahak117@gmail.com>
2026-06-14 23:55:25 +0530

    Time: 10 ms (6.94%), Space: 11 MB (8.39%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string ans= to_string(int (x));
+        int l=0;
+        int h=ans.length()-1;
+        while(l<=h){
+            if(x<0) return false;
+            else if(ans[l]!=ans[h]){
+                return false;
+            }
+            l++;
+            h--;
+        }
+        return true;
+    }
+};
